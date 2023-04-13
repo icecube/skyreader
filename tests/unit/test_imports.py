d249bd9abc20843c0d819abd6625ca3888f3d1e4
Ric Evans <19216225+ric-evans@users.noreply.github.com>
2023-04-13 15:04:26 -0500

    Package Infrastructure (#3)
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: wipacdevbot <developers@icecube.wisc.edu>

---
+++ tests/unit/test_imports.py
@@ -0,0 +1,11 @@
+"""Test objects are importable that should be importable."""
+
+
+import skyreader
+
+
+def test_skyreader_imports() -> None:
+    """Test importing from 'skyreader'."""
+    assert hasattr(skyreader, "result")
+    assert hasattr(skyreader, "SkyScanResult")
+    assert hasattr(skyreader, "plot")
