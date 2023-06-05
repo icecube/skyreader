bbd60ba936e4312f543c2fbfee800d1afbe073b6
Ric Evans <ric@evans-jacquez.com>
2023-06-05 14:36:12 -0700

    add `test_011__error`

--- tests/unit/test_is_close.py
+++ tests/unit/test_is_close.py
@@ -1,6 +1,7 @@
 """Test `is_close()`."""
 
 
+import pytest
 from skyreader import SkyScanResult
 from skyreader.result import PyDictResult
 
@@ -68,6 +69,57 @@ def test_010() -> None:
         rtol_per_field=rtol_per_field,
     )
     assert beta.is_close(
+        alpha,
+        equal_nan=True,
+        dump_json_diff=None,
+        do_disqualify_zero_energy_pixels=False,
+        rtol_per_field=rtol_per_field,
+    )
+
+
+@pytest.mark.parametrize("fail_field", ["llh", "E_in", "E_tot"])
+def test_011__error(fail_field: str) -> None:
+    """Compare two simple instances."""
+    rtol_per_field = dict(llh=0.5, E_in=0.5, E_tot=0.5)
+    increase = dict(llh=1.0, E_in=1.0, E_tot=1.0)  # >1 should fail
+    increase[fail_field] = 2.0
+
+    alpha_pydict: PyDictResult = {
+        "nside-8": {
+            "columns": ["index", "llh", "E_in", "E_tot"],
+            "metadata": {"nside": 8},
+            "data": [
+                [0, 496.5, 4643.5, 4736.5],
+            ],
+        },
+    }
+    alpha = SkyScanResult.deserialize(alpha_pydict)
+
+    beta_pydict: PyDictResult = {
+        "nside-8": {
+            "columns": ["index", "llh", "E_in", "E_tot"],
+            "metadata": {"nside": 8},
+            "data": [
+                [
+                    i[0],
+                    i[1] * (1 + increase["llh"] * rtol_per_field["llh"]),
+                    i[2] * (1 + increase["E_in"] * rtol_per_field["E_in"]),
+                    i[3] * (1 + increase["E_tot"] * rtol_per_field["E_tot"]),
+                ]
+                for i in alpha_pydict["nside-8"]["data"]
+            ],
+        },
+    }
+    beta = SkyScanResult.deserialize(beta_pydict)
+
+    assert not alpha.is_close(
+        beta,
+        equal_nan=True,
+        dump_json_diff=None,
+        do_disqualify_zero_energy_pixels=False,
+        rtol_per_field=rtol_per_field,
+    )
+    assert not beta.is_close(
         alpha,
         equal_nan=True,
         dump_json_diff=None,
