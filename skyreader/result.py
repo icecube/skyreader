998d53ac33226d7ea9cfc6c22aff986358c7c95f
Ric Evans <ric@evans-jacquez.com>
2023-06-07 15:17:11 -0700

    remove `do_disqualify_zero_energy_pixels`

--- skyreader/result.py
+++ skyreader/result.py
@@ -176,41 +176,23 @@ class SkyScanResult:
         sre_pix: np.ndarray,
         ore_pix: np.ndarray,
         equal_nan: bool,
-        do_disqualify_zero_energy_pixels: bool,  # TODO: remove?
         rtol_per_field: Dict[str, float],
     ) -> Tuple[List[float], List[bool]]:
         """Get the diff float-values and test truth-values for the 2 pixel-
         data.
 
         The datapoints are compared face-to-face (zipped).
-
-        If `do_disqualify_zero_energy_pixels=True` there's an
-        invalid datapoint value in either array, all "require close"
-        datapoints are considered (vacuously) close enough. # TODO: remove?
         """
         diff_vals = []
         test_vals = []
 
-        # is one of the pixel-datapoints is "so bad" it has
-        # disqualified all the other "require_close" datapoints?
-        is_pixel_disqualified = False  # TODO: remove?
-        if do_disqualify_zero_energy_pixels:  # TODO: remove?
-            is_pixel_disqualified = any(
-                sre_pix[self.PIXEL_FIELDS.index(f)] == 0.0
-                or ore_pix[self.PIXEL_FIELDS.index(f)] == 0.0
-                for f in ZERO_MAKES_FIELD_ALWAYS_ISCLOSE
-            )
-
         for s_val, o_val, field in zip(sre_pix, ore_pix, self.PIXEL_FIELDS):
             s_val, o_val = float(s_val), float(o_val)
 
-            # CASE 1: a disqualified-pixel "require close" datapoint
-            if field in rtol_per_field and is_pixel_disqualified:
-                diff, test = float("nan"), True  # vacuously true
-            # CASE 2: a "require close" datapoint (not disqualified-pixel)
-            elif field in rtol_per_field:
+            # CASE: a "require close" datapoint
+            if field in rtol_per_field:
                 diff, test = self.isclose_datapoint(s_val, o_val, field, equal_nan, rtol_per_field)
-            # CASE 3: a "require equal" datapoint
+            # CASE: a "require equal" datapoint
             else:
                 diff, test = s_val - o_val, s_val == o_val
 
@@ -247,7 +229,6 @@ class SkyScanResult:
     def isclose_nside(self,
         other: "SkyScanResult",
         equal_nan: bool,
-        do_disqualify_zero_energy_pixels: bool,  # TODO: remove?
         rtol_per_field: Dict[str, float],
         nside: str,
     ) -> Tuple[bool, List[Tuple[Tuple[Any, ...], Tuple[Any, ...], Tuple[float, ...], Tuple[bool, ...]]]]:
@@ -260,7 +241,7 @@ class SkyScanResult:
             fillvalue=np.full((len(self.PIXEL_FIELDS),), np.nan),  # 1 vector
         ):
             diff_vals, test_vals = self.isclose_pixel(
-                sre_pix, ore_pix, equal_nan, do_disqualify_zero_energy_pixels, rtol_per_field
+                sre_pix, ore_pix, equal_nan, rtol_per_field
             )
             pix_diff = (
                 tuple(sre_pix.tolist()),
@@ -296,7 +277,6 @@ class SkyScanResult:
         other: "SkyScanResult",
         equal_nan: bool = True,
         dump_json_diff: Optional[Path] = None,
-        do_disqualify_zero_energy_pixels: bool = False,  # TODO: remove?
         rtol_per_field: Optional[Dict[str, float]] = None,
     ) -> bool:
         """Checks if two results are close by requiring strict equality on
@@ -311,11 +291,6 @@ class SkyScanResult:
             `dump_json_diff`
                 get a json file containing every comparison at the pixel-data level
                 (default: `None`)
-            `do_disqualify_zero_energy_pixels`
-                If `do_disqualify_zero_energy_pixels=True` and there's an
-                invalid datapoint value in either array, all "require close"
-                datapoints are considered (vacuously) close enough. # TODO: remove?
-                (default: `False`)
             `rtol_per_field`
                 a mapping of each field to a rtol value
                 (default: `DEFAULT_RTOL_PER_FIELD`)
@@ -335,7 +310,7 @@ class SkyScanResult:
             # Q: why aren't we using np.array_equal and np.allclose?
             # A: we want detailed pixel-level diffs w/out repeating detailed code
             close[nside], diffs[nside] = self.isclose_nside(
-                other, equal_nan, do_disqualify_zero_energy_pixels, rtol_per_field, nside
+                other, equal_nan, rtol_per_field, nside
             )
 
         # finish up
