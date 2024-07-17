6a227c5b72426bca585fcea8f5e521f6cb50937c
G-Sommani <giacomo.sommani@gmail.com>
2024-07-17 17:42:45 +0200

    fixing area estimation with Gauss Green

--- skyreader/plot/plot.py
+++ skyreader/plot/plot.py
@@ -53,9 +53,9 @@ class SkyScanPlotter:
         a = 0
         x0, y0 = vs[0]
         for [x1,y1] in vs[1:]:
-            dx = x1-x0
-            dy = np.cos(y1)-np.cos(y0)
-            a += 0.5*(np.cos(y0)*dx - x0*dy)
+            dx = np.cos(x1)-np.cos(x0)
+            dy = y1-y0
+            a += 0.5*(y0*dx - np.cos(x0)*dy)
             x0 = x1
             y0 = y1
         return a
