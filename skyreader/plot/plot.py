fix normalization of rayleigh distribution

skyreader/plot/plot.py
@@ -527,10 +527,12 @@ class SkyScanPlotter:
         space_angle, ang_dist_grid = get_space_angles(
             min_ra, min_dec, grid_ra, grid_dec, max_nside, min_index
         )
-        equatorial_map += rayleigh(
+        gauss_values = rayleigh(
             space_angle,
             self.NEUTRINOFLOOR_SIGMA
         )
+        gauss_values = gauss_values / np.nansum(gauss_values)
+        equatorial_map += gauss_values
         #equatorial_map = np.where(
         #    equatorial_map > 1e-12, equatorial_map, 0.0
         #)