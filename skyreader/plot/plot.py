1d1d0ac908d532a2bf6120b489ea5e53d9fa4439
G-Sommani <giacomo.sommani@gmail.com>
2024-09-10 14:28:08 +0200

    clip at lower probabilities for all-sky map

--- skyreader/plot/plot.py
+++ skyreader/plot/plot.py
@@ -164,7 +164,7 @@ class SkyScanPlotter:
             max_llh = 50
         prob_map = copy.copy(grid_map) - min_llh
         prob_map = np.exp(-prob_map)
-        min_prob = 1e-32
+        min_prob = 1e-64
         max_prob = np.nanmax(prob_map)
         prob_map = prob_map.clip(min_prob, None).astype('float32')
 
