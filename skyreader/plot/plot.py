3da7e459cc5987c6240876f1625c480290839c69
G-Sommani <giacomo.sommani@gmail.com>
2024-10-02 18:46:24 +0200

    return to gz

--- skyreader/plot/plot.py
+++ skyreader/plot/plot.py
@@ -1016,7 +1016,7 @@ class SkyScanPlotter:
 
         # save flattened map
         healpy.write_map(
-            f"{unique_id}.skymap_nside_{mmap_nside}.fits.zstd",
+            f"{unique_id}.skymap_nside_{mmap_nside}.fits.gz",
             equatorial_map,
             coord='C',
             column_names=column_names,
