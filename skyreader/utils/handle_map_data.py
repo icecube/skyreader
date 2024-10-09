ea06ac862d7054ac25e7694ad139c1e635a9bf85
G-Sommani <giacomo.sommani@gmail.com>
2024-10-09 14:31:23 +0200

    no zeros allowed in probability map

--- skyreader/utils/handle_map_data.py
+++ skyreader/utils/handle_map_data.py
@@ -119,7 +119,7 @@ def extract_map(
             )
 
             # normalize map
-            min_map = np.nanmin(equatorial_map[equatorial_map >= 0.0])
+            min_map = np.nanmin(equatorial_map[equatorial_map > 0.0])
             equatorial_map[np.isnan(equatorial_map)] = min_map
             print(min_map)
             equatorial_map = equatorial_map.clip(min_map, None)
