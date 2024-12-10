3771d7d1e741ee52a5a1b46709045b68e8bd2f19
G-Sommani <giacomo.sommani@gmail.com>
2024-12-10 11:38:12 +0100

    get rid of nan values for llh maps only when drawing contours

--- skyreader/plot/plot.py
+++ skyreader/plot/plot.py
@@ -448,7 +448,10 @@ class SkyScanPlotter:
 
         if not circular:
             if llh_map:
-                grid_values_for_contours = grid_value
+                # get rid of nan values only for the contours and
+                # avoiding crashes during plotting
+                max_map = np.nanmax(equatorial_map)
+                grid_values_for_contours = grid_value.clip(None, max_map)
                 contour_levels_for_contours = contour_levels
             else:
                 grid_values_for_contours = np.log(grid_value)
