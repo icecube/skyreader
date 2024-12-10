900c61ab6b3f5156ea183d0013d57136d00c5613
G-Sommani <giacomo.sommani@gmail.com>
2024-12-10 15:27:53 +0100

    fix typo

--- skyreader/plot/plot.py
+++ skyreader/plot/plot.py
@@ -456,7 +456,9 @@ class SkyScanPlotter:
                 grid_values_for_contours[
                     np.isnan(grid_values_for_contours)
                 ] = max_map
-                grid_values_for_contours = grid_value.clip(None, max_map)
+                grid_values_for_contours = grid_values_for_contours.clip(
+                    None, max_map
+                )
                 contour_levels_for_contours = contour_levels
             else:
                 grid_values_for_contours = np.log(grid_value)
