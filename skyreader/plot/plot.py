815a0782784f67e629d28be57d62420d686704a3
G-Sommani <giacomo.sommani@gmail.com>
2025-01-21 15:58:46 +0100

    debug create_plot

--- skyreader/plot/plot.py
+++ skyreader/plot/plot.py
@@ -131,7 +131,8 @@ class SkyScanPlotter:
             map_to_plot[plotting_map != 0.] = np.log10(
                 plotting_map[plotting_map != 0.]
             )
-            #map_to_plot[plotting_map == 0.] = np.nan
+            print(np.min(map_to_plot), np.nanmin(map_to_plot))
+            map_to_plot[plotting_map == 0.] = np.nan
         equatorial_map = np.ma.masked_invalid(equatorial_map)
         map_to_plot = np.ma.masked_invalid(map_to_plot)
 
