efc07f2f005741952724664c5a40f3386130d0e1
G-Sommani <giacomo.sommani@gmail.com>
2025-01-21 16:55:11 +0100

    try fix

--- skyreader/plot/plot.py
+++ skyreader/plot/plot.py
@@ -133,13 +133,8 @@ class SkyScanPlotter:
             )
             print(np.min(map_to_plot), np.nanmin(map_to_plot))
             print(np.min(equatorial_map), np.nanmin(equatorial_map))
-            map_to_plot[plotting_map == 0.] = np.nan
-            map_to_plot[
-                np.logical_or(
-                    np.isnan(map_to_plot),
-                    map_to_plot == 0.
-                )
-            ] = np.nanmin(map_to_plot)
+            # map_to_plot[plotting_map == 0.] = np.nan
+            map_to_plot[np.isnan(map_to_plot)] = np.nanmin(map_to_plot)
         equatorial_map = np.ma.masked_invalid(equatorial_map)
         map_to_plot = np.ma.masked_invalid(map_to_plot)
 
