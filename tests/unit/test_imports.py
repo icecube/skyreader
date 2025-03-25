6dfcc37938d2bad18a1f46c4c18906a13f42b00f
Angela <angela@Computer-portatile-di-Angela.local>
2025-03-26 00:04:26 +0100

    different function to test fermi catalog import

--- tests/unit/test_imports.py
+++ tests/unit/test_imports.py
@@ -9,8 +9,11 @@ def test_skyreader_imports() -> None:
     assert hasattr(skyreader, "SkyScanResult")
     assert hasattr(skyreader, "plot")
 
+
+def fermi_catalog_import() -> None:
     expected_catalog_path = (
         "/cvmfs/icecube.opensciencegrid.org/users/azegarelli/realtime/"
         "catalogs/gll_psc_v35.fit"
     )
+
     assert CATALOG_PATH == expected_catalog_path
