6574a83137725203fc62fe67198641bb2a7695cb
Angela <angela@mobile-222.shadow2.airub.net>
2025-03-27 12:22:58 +0100

    add new files from branch update-fermi-catalog

--- tests/unit/test_imports.py
+++ tests/unit/test_imports.py
@@ -1,7 +1,5 @@
-"""Test objects are importable that should be importable."""
-
-
 import skyreader
+from skyreader.constants import CATALOG_PATH
 
 
 def test_skyreader_imports() -> None:
@@ -9,3 +7,12 @@ def test_skyreader_imports() -> None:
     assert hasattr(skyreader, "EventMetadata")
     assert hasattr(skyreader, "SkyScanResult")
     assert hasattr(skyreader, "plot")
+
+
+def fermi_catalog_import() -> None:
+    expected_catalog_path = (
+        "/cvmfs/icecube.opensciencegrid.org/users/azegarelli/realtime/"
+        "catalogs/gll_psc_v35.fit"
+    )
+
+    assert CATALOG_PATH == expected_catalog_path
