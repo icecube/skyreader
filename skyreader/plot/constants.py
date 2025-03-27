877c6d59fc85790675763cb13a5c33490fcdb79a
Angela <angela@mobile-222.shadow2.airub.net>
2025-03-27 12:35:52 +0100

    resolve conflicts

--- skyreader/plot/constants.py
+++
@@ -1,5 +0,0 @@
-# LAT 14-year Source Catalog (4FGL-DR4)
-# fermi.gsfc.nasa.gov/ssc/data/access/lat
-CATALOG_NAME = "gll_psc_v35.fit"
-BASE_DIR = "/cvmfs/icecube.opensciencegrid.org/users/"
-CATALOG_PATH = f"{BASE_DIR}azegarelli/realtime/catalogs/{CATALOG_NAME}"
