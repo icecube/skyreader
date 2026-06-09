b1d603e6c59ccb1ff7691769e5b412f4da8a2500
Angela Zegarelli <159175351+azegarelli@users.noreply.github.com>
2026-06-09 10:36:17 +0200

    fix flake8

--- skyreader/constants.py
+++ skyreader/constants.py
@@ -1,6 +1,6 @@
 # LAT 14-year Source Catalog (4FGL-DR4)
 # fermi.gsfc.nasa.gov/ssc/data/access/lat
-#CATALOG_NAME = "gll_psc_v35.fit"
+# CATALOG_NAME = "gll_psc_v35.fit"
 
 # LAT 16-year Source List (FL16Y)
 # https://fermi.gsfc.nasa.gov/ssc/data/access/lat/fl16y/
